86f1f3bcea55ff9b7f3f6c3f159da9f4ff503489
veepgoose <victoriaplatt@loveplanes.co.uk>
2023-06-24 17:12:49 +0100

    Header component added and imported to App.js, and Header.css imported

--- lotto/src/App.js
+++ lotto/src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header/Header.js';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <Header className="header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          I swear to god why is this so hard. FML
         </p>
         <a
           className="App-link"
@@ -15,9 +16,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Fucking React
         </a>
-      </header>
+      </Header>
     </div>
   );
 }

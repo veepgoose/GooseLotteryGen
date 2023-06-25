4898c265f277ca8b2a3f8cd485d004948b64ad9c
Goose <victoriaplatt@loveplanes.co.uk>
2023-06-25 14:57:35 +0100

    text input field added to card component and styled

--- lotto/src/Components/Card/Card.js
+++ lotto/src/Components/Card/Card.js
@@ -6,6 +6,7 @@ const Card = () => {
     <div className="card">
       <h2>Card Title</h2>
       <p>This is the card content.</p>
+      <input className="name-field" type="text" placeholder="Gimme your name"/>
     </div>
   );
 };

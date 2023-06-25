a2ab427f5fbe01517eea90014978f66bd2ba3941
Goose <victoriaplatt@loveplanes.co.uk>
2023-06-25 15:18:36 +0100

    Header logo added and styled

--- lotto/src/Components/Header/Header.js
+++ lotto/src/Components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Header.css'
+import gooseLogo from './gooseLogo.png';
 
 function Header() {
     return (
       <header className="header">
-        <h1>High Goose Lottery Bollocks</h1>
+      <img src={gooseLogo} alt="Goose logo" className="gooseLogo"/>
+       
       </header>
     );
   }

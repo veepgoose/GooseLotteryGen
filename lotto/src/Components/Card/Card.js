9fb3b810aa666fa8bfad209b0caef0ad37e8cc70
Goose <victoriaplatt@loveplanes.co.uk>
2023-06-26 09:51:45 +0100

    Card component edited

--- lotto/src/Components/Card/Card.js
+++ lotto/src/Components/Card/Card.js
@@ -4,8 +4,8 @@ import "./card.css";
 const Card = () => {
   return (
     <div className="card">
-      <h2>Card Title</h2>
-      <p>This is the card content.</p>
+      <h2>WELCOME</h2>
+      <p>To The High Goose Lottery Number Generator</p>
       <input className="name-field" type="text" placeholder="Gimme your name"/>
     </div>
   );
